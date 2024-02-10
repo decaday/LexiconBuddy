e19bf8615946abde76122e0b19cbbd86e32923e8
decaday <myDecaday@outlook.com>
2024-02-10 17:13:22 +0800

    refactor dict

--- binary_search/src/main.rs
+++ binary_search/src/main.rs
@@ -1,44 +1,65 @@
 use std::fs::File;
-use std::io::{BufRead, BufReader};
-use std::io::SeekFrom;
 use std::io::prelude::*;
+use std::io::SeekFrom;
+use std::io::{BufRead, BufReader};
 
 use std::error::Error;
-use crate::DictTypes::Ecdict;
 
-// Dictionary Types
+
+/** Dictionary types
+Ecdict, Oxford
+ */
 enum DictTypes {
     Ecdict,
     Oxford,
 }
 
+#[derive(thiserror::Error, Debug)]
+enum DictError {
+    #[error("Word not found")]
+    WordNotFound,
+    #[error(transparent)]
+    IOError(#[from] std::io::Error),
+    #[error("Parse error in `{0}`")]
+    ParseError(&'static str),
+    #[error("Parse int error")]
+    ParseIntError(#[from] std::num::ParseIntError),
+}
 
-struct Dict{
+/** Dict
+*/
+struct Dict {
     index_file: File,
     index_file_size: u64,
     dict_type: DictTypes,
 }
-impl Dict{
-    fn build(dict_type: DictTypes) -> Result<Dict, Box<dyn Error>>{
+impl Dict {
+    fn build(dict_type: DictTypes) -> Result<Dict, DictError> {
         let mut index_file = File::open("././index.txt")?;
         let metadata = index_file.metadata()?;
         let index_file_size = metadata.len();
-        Ok(Dict{
+        Ok(Dict {
             index_file,
             index_file_size,
-            dict_type
+            dict_type,
         })
     }
 
-    fn search_index(mut self, entry: &str) -> Result<u64, Box<dyn Error>> {
+    /** Use binary search to find the address of the word in the index.
+     */
+    fn search_index(mut self, entry: &str) -> Result<u64, DictError> {
         let mut low = 0;
         let mut high = self.index_file_size;
         let mut i = 0;
+        let mut last_pos: u64 = 0;
+
         while low <= high {
             i = i + 1;
             if i > 30 {
-                break
+                break;
             }
+
+            //Calculate the middle location address
             let mid = low + (high - low) / 2 + 1;
             let mut current_pos = mid;
             self.index_file.seek(SeekFrom::Start(current_pos))?;
@@ -48,7 +69,7 @@ impl Dict{
             let mut pos = current_pos;
             loop {
                 if pos < low {
-                    break
+                    break;
                 }
                 self.index_file.read_exact(&mut ch)?;
                 if ch[0] == b'\n' {
@@ -58,63 +79,75 @@ impl Dict{
                 self.index_file.seek(SeekFrom::Start(pos))?;
             }
             current_pos = pos + 1;
+            if last_pos == current_pos {
+                //enters a dead end, indicating that the target word does not exist
+                break;
+            }
+            last_pos = current_pos;
             self.index_file.seek(SeekFrom::Start(current_pos))?;
 
             // Read the line
             let mut line = String::new();
+            self.index_file.read_exact(&line);
+            let mut line = String::new();
             let mut reader = BufReader::<&File>::new(&self.index_file);
             reader.read_line(&mut line)?;
-            let parts: Vec<&str> = line.split(',').collect();
-            let current_word = parts[0];
+            let mut parts = line.split(',');
+
+            let current_word = match parts.next() {
+                Some(part) => part,
+                None => return Err(DictError::ParseError("part 1")),
+            };
             println!("current word: {current_word}");
+
             match current_word.cmp(&entry) {
                 std::cmp::Ordering::Less => low = mid + 1,
                 std::cmp::Ordering::Greater => high = mid - 1,
                 std::cmp::Ordering::Equal => {
-                    let addr = parts[1].trim().parse()?;
+                    let addr = match parts.next() {
+                        Some(part) => part,
+                        None => return Err(DictError::ParseError("part 2")),
+                    }
+                    .trim().parse()?;
                     return Ok(addr);
                 }
             }
         }
-        Err("Target word not found in file".into())
+        Err(DictError::WordNotFound)
     }
 }
 
+// struct EcdictWord {
+//     word: String,
+//     phonetic: String,
+//     translation: String,
+//     tag: String,
+// }
 
+// impl EcdictWord {
+//     fn build(entry: &str) -> Result<(), Box<dyn Error>> {
+//         Ok(())
+//     }
+// }
 
-struct EcdictWord{
-    word: String,
-    phonetic: String,
-    translation: String,
-    tag: String,
-}
-
-
-
-impl EcdictWord{
-    fn build(entry: &str) -> Result<(), Box<dyn Error>>{
-        Ok(())
-    }
-}
-
-
-fn run() -> Result<u64, Box<dyn Error>>{
+fn run() -> Result<u64, DictError> {
     let entry = "apple";
-    let ecdict = Dict::build(Ecdict)?;
+    let ecdict = Dict::build(DictTypes::Ecdict)?;
     let addr = ecdict.search_index(&entry)?;
     // let word = EcdictWord::build(entry)?;
     Ok(addr)
 }
 
-
 fn main() {
-
     match run() {
         Ok(addr) => {
             println!("Addresses for it: {addr}");
-        },
-        Err(error) => println!("Error: {error}")
+        }
+        Err(error) => match error {
+            DictError::IOError(e) => println!("IO error! {e}"),
+            DictError::ParseError(e) => println!("ParseError {e}"),
+            DictError::ParseIntError(e) => println!("ParseError {e}"),
+            DictError::WordNotFound => println!("Word not fond"),
+        }
     };
-
-
 }
